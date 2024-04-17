b567cc27379541bb0a979a9c0355604356aa2863
Dreamy-Z3r0 <34434717+Dreamy-Z3r0@users.noreply.github.com>
2024-04-17 15:12:24 +0700

    Add files via upload

---
+++ Build.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+import PyInstaller.__main__
+
+if __name__ == '__main__':
+    if not os.path.isfile('container.py'):
+        exec(open('MissingContainer.py').read())
+
+    if os.path.isfile('DumbApp.exe'):
+        os.remove('DumbApp.exe')
+
+    PyInstaller.__main__.run([
+        'DumbApp.py',
+        '--onefile',
+        '--distpath',
+        '.\\'
+    ])
+
+    shutil.rmtree('.\\build')
+    os.remove('DumbApp.spec')
