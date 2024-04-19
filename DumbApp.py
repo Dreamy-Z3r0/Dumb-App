8efd80b240670b69cd0accccf9b549d86535387f
Dreamy-Z3r0 <34434717+Dreamy-Z3r0@users.noreply.github.com>
2024-04-19 10:43:12 +0700

    Add files via upload

--- DumbApp.py
+++ DumbApp.py
@@ -1,41 +1,122 @@
 import sys
 from container import *
+from functools import partial
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import (
+    QApplication, QMainWindow, 
+    QWidget, QLabel, QPushButton,
+    QVBoxLayout
+)
+from PyQt5.QtGui import QPixmap, QFontMetrics
 from PyQt5.QtCore import QTimer
+import PyQt5.QtCore as QtCore
 
 class MainWindow(QMainWindow):
     def __init__(self):
-        self.index = 0
-
         super(MainWindow, self).__init__()
-        self.setWindowTitle("Don't get bored...")
-
-        self.label = QLabel()
-        self.setCentralWidget(self.label)
 
         # Create a QTimer
         self.timer = QTimer()
-        self.timer.setInterval(50)  # Trigger every 1 second (1000 ms)
 
-        # Connect the QTimer timeout signal to our update function
+        # Load main window contents
+        self.Welcome_Menu()
+        
+
+    def Welcome_Menu(self):
+        self.setWindowTitle('Dumb App')
+
+        layout = QVBoxLayout()
+
+        self.label = QLabel('DUMB APP')
+        self.label.setAlignment(QtCore.Qt.AlignHCenter)
+        layout.addWidget(self.label)
+
+        buttons = []
+        buttonLabels = []
+
+        buttonLabels.append('Snake game')
+        buttonLabels.append('Surprisingly suspicious')
+        
+        for i, buttonLabel in enumerate(buttonLabels):
+            buttons.append(QPushButton(buttonLabel))
+            buttons[-1].setCheckable(True)
+            buttons[-1].clicked.connect(partial(self.WelcomeMenu_ButtonRoutines,buttonLabel))
+
+            layout.addWidget(buttons[-1])
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+        self.adjustSize()
+
+    def WelcomeMenu_ButtonRoutines(self, buttonLabel):
+        if buttonLabel == 'Main':
+            try:
+                self.timer.stop()
+            except:
+                pass
+            finally:
+                self.Welcome_Menu()
+        else:
+            if buttonLabel == 'Surprisingly suspicious':
+                self.Sus_Window()
+            else:
+                pass
+
+    def Snake_Game(self):
+        self.setWindowTitle('Snake Game')
+
+        layout = QVBoxLayout()
+
+    def Sus_Window(self):
+        self.setWindowTitle("Don't get bored...")
+        self.index = 0
+
+        layout = QVBoxLayout()
+
+        button = QPushButton('Back')
+        font_metrics = QFontMetrics(button.font())
+        textWidth = font_metrics.width(button.text())
+        textHeight = font_metrics.height()
+
+        button.setFixedWidth(textWidth + 20)
+        button.setFixedHeight(textHeight + 10)
+
+        button.setCheckable(True)
+        button.clicked.connect(partial(self.WelcomeMenu_ButtonRoutines, 'Main'))
+        layout.addWidget(button)
+
+        self.update_label()
+        layout.addWidget(self.label)
+
+        # Stop timer (if running)
+        try:
+            self.timer.stop()
+        except:
+            pass
+
+        # Update refresh rate -> 20Hz
+        self.timer.setInterval(50)
+
+        # Connect the QTimer timeout signal
         self.timer.timeout.connect(self.update_label)
 
         # Start the timer
         self.timer.start()
 
-        # Update the time display immediately
-        self.update_label() 
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
 
     def update_label(self):
         pixmap = QPixmap()
         pixmap.loadFromData(frames[self.index])
+
         self.label.setPixmap(pixmap)
 
         self.index += 1
         if self.index == len(frames):
-            self.index = 2
+            self.index = 0
 
 
 if __name__ == '__main__':
